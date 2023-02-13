1851a6af58f2a5619a7d77855be13e6c070b3fdd
kamal1234567890 <kamaluddin1623@gmail.com>
2023-02-13 20:39:32 +0900

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <body className="bg-slate-900">
+
+    <div className="container mx-auto text-slate-400">
+      <div className="flex justify-center items-center h-screen">
+        <section className="bg-gray-800 w-96 h-96 border-2 border-slate-400 rounded-lg p-4 shadow-lg shadow-slate-800">
+          <h1 className="text-3xl font-bold underline text-center">
+            Hello world!
+          </h1>
+          <section className="mt-4 text-justify">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quis earum corrupti repudiandae vitae ducimus quibusdam nisi asperiores quas, dolore cumque, doloremque, quae voluptas non numquam velit blanditiis amet nobis.</p>
+          </section>
+        </section>
+      </div>
+    </div>
+    </body>
+  );
+}
+
+export default App;
